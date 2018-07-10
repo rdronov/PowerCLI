9444a564f34a2c98afb0d16d3721992fa384bdf3
rdronov <rdronov@gmail.com>
2018-07-10 19:41:32 +1000

    Update Get-DirectPathIOStatus.ps1
    Version 1.1
    #  Added the PowerCLI version check
    # Improved the host connection block
    # Added an export to CSV

--- Get-DirectPathIOStatus.ps1
+++ Get-DirectPathIOStatus.ps1
@@ -2,30 +2,34 @@
 # Description: VMXNET3 - Network DirectPath I/O Status
 # VMware KB: https://kb.vmware.com/kb/2145889
 #
-# 06/07/2018 - Version 1.0.1 - Minor changes to check PowerCLI version
-# 26/04/2017 - Version 1.0 - Initial Release
+# 06/07/2018 - Version 1.1
+#     - Added the PowerCLI version check
+#     - Improved the host connection block
+#     - Added an export to CSV
+# 26/04/2017 - Version 1.0
+#     - Initial Release
 #
 # Author: Roman Dronov (c)
 
 
 # Initial variables
-$OutputCollection = @()
+$outputCollection = @()
 $newModule = "VMware.PowerCLI"
 $oldModule = "VMware.VimAutomation.Core"
 
 
 # Check the PowerCLI module availability
 Clear-Host
-Write-Host "`n  Checking if the VMware PowerCLI module exists...`r"
+Write-Host "`n Checking if the VMware PowerCLI modules are loaded...`r"
 
-if($(Get-InstalledModule | ? {$_.Name -like $newModule}) -eq $null) {
+if ($(Get-InstalledModule | ? {$_.Name -like $newModule}) -eq $null) {
     
-    if ($(Get-Module | ? {$_.Name -like $oldModule}) -eq $null -and $(Get-Module -Name $oldModule -ListAvailable -ErrorAction SilentlyContinue) -eq $null) {
+    if ($(Get-Module | ? {$_.Name -like $oldModule}) -eq $null -and $(Get-Module -Name $oldModule -ListAvailable -ErrorAction Ignore) -eq $null) {
         Write-Host "`n  You need to install the VMware PowerCLI module to run this script. Exiting...`r" -ForegroundColor Yellow
         exit
     }
     else {
-        Import-Module -Name $oldModule -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+        Import-Module -Name $oldModule -ErrorAction Ignore -WarningAction Ignore
         $moduleVersion = $(Get-Module -Name $oldModule).Version
         Write-Host "`n  The VMware PowerCLI module version $moduleVersion is loaded successfuly.`r" -ForegroundColor Green
     }   
@@ -36,39 +40,114 @@ else {
 }
 
 
-# Connect to the vCenter server
-$VIServer = Read-Host -Prompt " Input the vCenter server name and then press Enter"
-Connect-VIServer -Server $VIServer -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Out-Null
+# Set the VIServer connection mode to Multiple
+$connectionMode = $(Get-PowerCLIConfiguration -Scope Session).DefaultVIServerMode
+
+if ($connectionMode -like 'Single') {
+    Set-PowerCLIConfiguration -Scope Session -DefaultVIServerMode Multiple -Confirm:$false | Out-Null
+}
+
+
+# Connect to the host(s)
+function Enter-Credentials {
+    $defaultUser = $env:USERDOMAIN + "\" + $env:USERNAME
+    if ($(Read-Host "`n Username [default: $($defaultUser)]") -eq '') {
+        $viUser = $defaultUser
+    }
+    else {
+        $viUser
+    }
+    $viPassword = Read-Host " Password" -AsSecureString
+    $script:viCredential = New-Object System.Management.Automation.PSCredential ($viUser,$viPassword)
+}
+
+function Verify-ViServer {
+    $viServer = Read-Host -Prompt "`n Input the host name and then press Enter"
+    
+    Write-Host "`n  Checking connection to ""$viServer""..." -ForegroundColor Green       
+    while ($(Test-Connection -ComputerName $viServer -Count 2 -ErrorAction Ignore) -eq $null) {
+        Write-Host "`n  Host ""$viServer"" is not reachable." -ForegroundColor Yellow
+        $viServer = Read-Host -Prompt "`n Input the correct host name and then press Enter"
+    }
+    $script:viServer = $viServer
+}
+
+if ($global:DefaultVIServers.Count -ne '0'){
+    
+    Write-Host "`n Connection has already established with the following host(s):"
+    $vcsaConnections = $global:DefaultVIServers
+    foreach ($vcsaServer in $vcsaConnections) {
+        Write-Host "`t*  $vcsaServer`r" -ForegroundColor Yellow
+    }
+    
+    $userAnswer = Read-Host "`n Would you like to continue using current connection(s)? [Yes/No]"
+    while ("Yes","No" -notcontains $userAnswer) {
+        $userAnswer = Read-Host "`n Please choose correct answer [Yes/No]"
+    }
+    
+    if ($userAnswer -eq 'No') {
+        Verify-ViServer
+        Enter-Credentials
+        
+        Write-Host "`n  Connecting to ""$viServer""..." -ForegroundColor Green
+        Connect-VIServer -Server $viServer -Credential $viCredential -ErrorAction Ignore -WarningAction Ignore | Out-Null
+    }
+    else {
+        continue
+    }
+}
+else {
+    Verify-ViServer
+    Enter-Credentials
+    
+    Write-Host "`n  Connecting to ""$viServer""..." -ForegroundColor Green
+    Connect-VIServer -Server $viServer -Credential $viCredential -ErrorAction Ignore -WarningAction Ignore | Out-Null
+}
+
 
 # Read all VMs that have VMXNET3 
-$VMNetworks = Get-VM | Get-NetworkAdapter | Where-Object {$_.Type -eq "Vmxnet3"}
+$vmNetworks = Get-VM | Get-NetworkAdapter | Where-Object {$_.Type -eq 'Vmxnet3'}
+
 
 # Get information about the network card
-For ($i=0; $i -lt $VMNetworks.Count; $i++) {
-    $VM = $VMNetworks[$i].Parent
-    $VMNetworkName = $VMNetworks[$i].NetworkName
-    $VMNetworkAdapter = $VMNetworks[$i].ExtensionData.DeviceInfo.Label
-    $VMNetworkAdapterDPIOStatus = $VMNetworks[$i].ExtensionData.UptCompatibilityEnabled
+for ($i=0; $i -lt $vmNetworks.Count; $i++) {
+    $vm = $vmNetworks[$i].Parent
+    $vmNetworkName = $vmNetworks[$i].NetworkName
+    $vmNetworkAdapter = $vmNetworks[$i].ExtensionData.DeviceInfo.Label
+    $vmNetworkAdapterDPIOStatus = $vmNetworks[$i].ExtensionData.UptCompatibilityEnabled
 
-    If ($VMNetworkAdapterDPIOStatus -ne "True") {$DPIOStatus = "Disabled"}
-    Else {$DPIOStatus = "Enabled"}
-
-        $Properties = @{'Virtual Machine' = $VM.Name;
-                        'HW Version' = $VM.Version;
-                        'Network Adapter' = $VMNetworkAdapter;
-                        'Network Name' = $VMNetworkName
-                        'DPIO Status' = $DPIOStatus
-                        }
-
-    $Object = New-Object –TypeName PSObject –Prop $Properties
-
-    $OutputCollection += $Object
+    if ($vmNetworkAdapterDPIOStatus -ne 'True') {
+        $dpioStatus = "Disabled"
+    }
+    else {
+        $dpioStatus = "Enabled"
     }
 
-# Disconnect from vCenter Server and clear the screen
-Disconnect-VIServer -Server $VIServer -Confirm:$false
+    $properties = @{'Virtual Machine' = $vm.Name;
+                    'HW Version' = $vm.Version;
+                    'Network Adapter' = $vmNetworkAdapter;
+                    'Network Name' = $vmNetworkName;
+                    'DPIO Status' = $dpioStatus}
+
+    $object = New-Object –TypeName PSObject –Property $properties
+
+    $outputCollection += $object
+    }
+
+
+# Disconnect from the host(s) and clear the screen
+Disconnect-VIServer * -Confirm:$false
 Clear-Host
 
+
 # Print out the results
-Write-Host "`n`tDirectPath I/O status`n"
-$OutputCollection | Sort-Object -Property "Virtual Machine" | Format-Table Virtual*,*Version,*Adapter,*Name,*Status -AutoSize
+$dpioResults = $outputCollection | Sort-Object -Property "Virtual Machine" | select Virtual*,*Version,*Adapter,*Name,*Status
+
+Write-Host "`n DirectPath I/O status`r"
+$dpioResults
+
+
+# Save results to CSV
+$csvPath = $env:TEMP + "\DPIOStatus-" + $(Get-Date -Format 'ddMMyyyy') + ".csv"
+$dpioResults | Export-CSV -Path $csvPath -NoTypeInformation -Force -Confirm:$false
+Invoke-Item $csvPath
