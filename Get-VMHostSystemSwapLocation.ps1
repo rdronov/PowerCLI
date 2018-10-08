b2224fac35fe039bd13c229b3c3530e855f9447c
rdronov <rdronov@gmail.com>
2018-10-08 16:40:28 +1100

    Update Get-VMHostSystemSwapLocation.ps1

--- Get-VMHostSystemSwapLocation.ps1
+++ Get-VMHostSystemSwapLocation.ps1
@@ -1,13 +1,14 @@
 # File name: Get-VMHostSystemSwapLocation.ps1
-# Description: This script provides information about datastore usage.
+# Description: This script provides information about the system swap location for ESXi hosts.
 #
 # 08/10/2018 - Version 1.0
 #     - Initial Release (based on https://code.vmware.com/forums/2530/vsphere-powercli#534332)
 #
 # Author: Roman Dronov (c)
 
+Clear-Variable vhost,esxcli2 -Scope Global | Out-Null
 
-Clear-Variable vhost -Scope Global
+Write-Host "`nSystem swap location:`r" -ForegroundColor Green
 
 ForEach ($vhost in $(Get-VMHost | sort Name)) {
 
@@ -15,9 +16,9 @@ ForEach ($vhost in $(Get-VMHost | sort Name)) {
 
   $esxcli2.sched.swap.system.get.Invoke() | Select `
   @{N='Host Name';E={$vhost.Name}}, `
+  @{N='Datastore Name';E={$_.DatastoreName}}, `
   @{N='Datastore Active';E={(Get-Culture).TextInfo.ToTitleCase($_.DatastoreActive)}}, `
-  @{N='Datastore Enabled';E={(Get-Culture).TextInfo.ToTitleCase($_.DatastoreEnabled)}}, `
-  @{N='Datastore Name';E={$_.DatastoreName}}
+  @{N='Datastore Enabled';E={(Get-Culture).TextInfo.ToTitleCase($_.DatastoreEnabled)}}
 
 }
 
